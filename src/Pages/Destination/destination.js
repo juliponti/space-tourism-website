c460d0b7c0b5eebccd49792fc9a9ba4f569278b3
Julieta <julieta.pontino@gmail.com>
2022-01-04 13:43:13 -0300

    wip destination section

--- src/Pages/Destination/destination.js
+++ src/Pages/Destination/destination.js
@@ -1,8 +1,8 @@
 import Header from "../../components/Header/header";
 import { useState } from "react";
-import Data from "../../data.json";
+import { data } from "../../data";
 
-const { destinations } = Data;
+const { destinations } = data;
 
 export default function Destination() {
   const [selectedPlanet, setSelectedPlanet] = useState(destinations[0]);
@@ -15,11 +15,15 @@ export default function Destination() {
         DESTINATION
       </h1>
       <div>
-        <img src={selectedPlanet.images.webp} alt="" />
-        <ul>
+        <img
+          src={selectedPlanet.images.webp}
+          alt={selectedPlanet.name}
+          className="absolute left-56 top-[343px] h-[445px] w-[445px]"
+        />
+        <ul className="flex flex-row absolute right-[11rem] bottom-[35rem]">
           {destinations.map((planet) => (
             <li
-              className="text-white"
+              className="text-details font-BarlowCo mr-9 uppercase text-base tracking-navbar cursor-pointer leading-9"
               onClick={() => setSelectedPlanet(planet)}
               key={planet.name}
             >
@@ -27,18 +31,31 @@ export default function Destination() {
             </li>
           ))}
         </ul>
-        <div>
-          <h2>{selectedPlanet.name}</h2>
-          <p>{selectedPlanet.description}</p>
+        <div className="flex flex-col absolute bottom-[260px] right-[3.6rem]">
+          <h2 className="font-Belle text-8xl text-white uppercase leading-[145px]">
+            {selectedPlanet.name}
+          </h2>
+          <p className="text-lg text-details w-[444px] leading-8">
+            {selectedPlanet.description}
+          </p>
         </div>
-        <div />
-        <div>
-          <h3>AVG. DISTANCE</h3>
-          <span>{selectedPlanet.distance}</span>
+        <div className="border bg-white w-[444px] absolute bottom-[13rem] right-[3.6rem] opacity-10" />
+
+        <div className=" absolute bottom-[6rem] right-[307px] leading-[17px] tracking-navbar">
+          <h3 className="text-details tracking-navbar w-fit text-[14px] font-BarlowCo">
+            AVG. DISTANCE
+          </h3>
+          <span className="font-Belle text-[28px] text-white leading-loose uppercase">
+            {selectedPlanet.distance}
+          </span>
         </div>
-        <div>
-          <h3>AVG. DISTANCE</h3>
-          <span>{selectedPlanet.travel}</span>
+        <div className=" absolute bottom-[6rem] right-[80px] leading-[17px] tracking-navbar">
+          <h3 className="text-details tracking-navbar w-fit text-[14px] font-BarlowCo">
+            EST. TRAVEL TIME
+          </h3>
+          <span className="font-Belle text-[28px] text-white leading-loose uppercase">
+            {selectedPlanet.travel}
+          </span>
         </div>
       </div>
     </div>
