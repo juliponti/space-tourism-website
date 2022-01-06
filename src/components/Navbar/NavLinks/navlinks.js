65df9aa3797f23cfc0416f7b8ad3fdf3960ecc71
Julieta <julieta.pontino@gmail.com>
2022-01-06 18:43:09 -0300

    improve navbarlinks

--- src/components/Navbar/NavLinks/navlinks.js
+++ src/components/Navbar/NavLinks/navlinks.js
@@ -1,48 +1,23 @@
 import { NavLink } from "react-router-dom";
-//import { useState } from "react";
-//import Destination from "../../../Pages/Destination/destination";
-//import Data from "../../../data.json";
+import { useState } from "react";
 
-function NavLinks() {
+function NavLinks(props) {
+  const { link, number, title } = props;
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <ol className="flex justify-evenly items-center w-full h-full list-none ">
-      <li>
-        <NavLink
-          to="/"
-          className="flex h-24 items-center no-underline text-white hover:border-b-[3px] hover:border-b-gray-400 hover:cursor-pointer active:border-b-[3px] active:border-b-white active:cursor-pointer"
-        >
-          <span className="nav-font-style mr-[10px] font-bold ">00</span>
-          <span className="nav-font-style">HOME</span>
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/destination"
-          className="flex h-24 items-center no-underline text-white hover:border-b-[3px] hover:border-b-gray-400 hover:cursor-pointer active:border-b-[3px] active:border-b-white active:cursor-pointer"
-        >
-          <span className="nav-font-style mr-[10px] font-bold ">01</span>
-          <span className="nav-font-style">DESTINATION</span>
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/crew"
-          className="flex h-24 items-center no-underline text-white hover:border-b-[3px] hover:border-b-gray-400 hover:cursor-pointer active:border-b-[3px] active:border-b-white active:cursor-pointer"
-        >
-          <span className="nav-font-style mr-[10px] font-bold ">02</span>
-          <span className="nav-font-style">CREW</span>
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/technology"
-          className="flex h-24 items-center no-underline text-white hover:border-b-[3px] hover:border-b-gray-400 hover:cursor-pointer active:border-b-[3px] active:border-b-white active:cursor-pointer"
-        >
-          <span className="nav-font-style mr-[10px] font-bold ">03</span>
-          <span className="nav-font-style">TECHNOLOGY</span>
-        </NavLink>
-      </li>
-    </ol>
+    <li>
+      <NavLink
+        to={link}
+        onClick={() => setIsActive(!isActive)}
+        className={`flex h-24 items-center no-underline text-white  hover:border-b-[3x] hover:border-b-gray-400 hover:cursor-pointer active:border-b-[3px] active:border-b-white active:cursor-pointer ${
+          isActive && "border-b-2 border-b-white"
+        }`}
+      >
+        <span className="nav-font-style mr-[10px] font-bold">{number}</span>
+        <span className="nav-font-style">{title}</span>
+      </NavLink>
+    </li>
   );
 }
 
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -11,7 +11,12 @@ function Navbar() {
       />
       <nav className="bg-[#ffffff08] backdrop-blur-lg absolute top-0 right-0 h-24 w-[830px]">
         <div className="h-0.5 w-[473px] border border-white absolute opacity-25 -left-[440px] top-12 z-10"></div>
-        <NavLinks />
+        <ol className="flex justify-evenly items-center w-full h-full list-none ">
+          <NavLinks link="/" number="00" title="HOME" />
+          <NavLinks link="/destination" number="01" title="DESTINATION" />
+          <NavLinks link="/crew" number="02" title="CREW" />
+          <NavLinks link="/technology" number="03" title="TECHNOLOGY" />
+        </ol>
       </nav>
     </>
   );
