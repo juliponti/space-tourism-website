c5694efb32b6813622ba021a9351f736df81c83b
Julieta <julieta.pontino@gmail.com>
2022-01-21 16:17:04 -0300

    destination section desktop

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,8 +4,12 @@ import MobileNavigation from "./mobilenavigation.js";
 
 function Navbar() {
   return (
-    <div className="md:w-full md:flex md:justify-between md:p-2 lg:p-8 lg:items-center">
-      <img src={NavIcon} className="lg:w-10 lg:h-10" alt="star logo" />
+    <div className="w-full flex justify-between p-2 lg:p-8 md:items-center">
+      <img
+        src={NavIcon}
+        className="md:w-12 md:h-12 lg:w-10 lg:h-10"
+        alt="star logo"
+      />
       <Navigation />
       <MobileNavigation />
     </div>
