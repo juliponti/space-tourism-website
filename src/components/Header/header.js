4bc1bb2cf8dd4ca093bc621418daaaecdeba043c
Julieta <julieta.pontino@gmail.com>
2022-02-01 16:11:52 -0300

    upgrade home section

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,7 +2,7 @@ import Navbar from "../../components/Navbar/navbar.js";
 
 function Header() {
   return (
-    <header className="flex justify-between w-full p-[0.7rem] md:p-0 lg:h-36 lg:pr-0 lg:after:absolute lg:after:top-[8%] lg:after:right-[800px] lg:after:w-[calc(100%-965px)] lg:after:border-t lg:after:border-white lg:after:opacity-25">
+    <header className="relative flex justify-between w-full p-[0.7rem] after:absolute after:top-[50%] after:right-[800px] after:w-[calc(100%-965px)] after:border-t lg:after:border-white after:opacity-25 md:p-0 lg:h-36 lg:pr-0">
       <Navbar />
     </header>
   );
